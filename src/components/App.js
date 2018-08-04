0bd1fb0ce0e349d9696c163560f803178d56a5ef
Grant Glidewell <ggcadc@gmail.com>
2018-08-04 14:30:49 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+// import './styles.css'
+
+export class App extends Component {
+  render() {
+    return <p>Hi, I've been packed, and transpiled</p>
+  }
+}
